c763b6a4306df5ac375c689f0b44dc6a3b7bc14e
Manchick <manchicking@gmail.com>
2026-05-17 11:42:39 +0200

    Reogranize Files

--- src/script.js
+++ src/script.js
@@ -1,28 +1,33 @@
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
 
+import first from "../assets/sheet/first.png";
+import second from "../assets/sheet/second.png";
+import third from "../assets/sheet/third.png";
+import fourth from "../assets/sheet/fourth.png";
+
 gsap.registerPlugin(SplitText);
-const letter = new SplitText(".letter", { type: "chars" });
+const content = new SplitText(".letter", { type: "chars" });
 const timeline = gsap.timeline();
 
 timeline
-    .to(".letter", {
+    .from(".letter", {
         duration: 0.5,
-        backgroundImage: () => 'url("assets/sheets/first.png")',
+        backgroundImage: () => `url(${first})`,
     })
     .to(".letter", {
         duration: 0.5,
-        backgroundImage: () => 'url("assets/sheets/second.png")',
+        backgroundImage: () => `url(${second})`,
     })
     .to(".letter", {
         duration: 0.5,
-        backgroundImage: () => 'url("assets/sheets/third.png")',
+        backgroundImage: () => `url(${third})`,
     })
     .to(".letter", {
         duration: 0.5,
-        backgroundImage: () => 'url("assets/sheets/fourth.png")',
+        backgroundImage: () => `url(${fourth})`,
     })
-    .from(letter.chars, {
+    .from(content.chars, {
         duration: 0.05,
         opacity: 0,
         stagger: 0.1,
